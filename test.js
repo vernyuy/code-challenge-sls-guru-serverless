f6d8f33810a5f37abaac66769e4a92e07dc6df47
vernyuy <fonchu.e.venyuy@gmail.com>
2024-02-22 18:04:15 +0100

    createItem function

---
+++ test.js
@@ -0,0 +1,30 @@
+'use strict'
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+const uuid = require('uuid');
+const TABLE_NAME = process.env.TABLE_NAME;
+
+
+module.exports.handler = async (event) => {
+  console.log(event);
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: TABLE_NAME,
+    Item: {
+      id: uuid.v4(),
+      ...data
+    }
+  };
+  
+  await docClient.put(params).promise();
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      message: 'Item inserted successfully'
+    })
+  };
+};
