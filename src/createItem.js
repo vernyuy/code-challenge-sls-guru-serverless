f4002ce3030cfbbb8761c4dbec335dc3116388af
vernyuy <fonchu.e.venyuy@gmail.com>
2024-02-22 15:57:25 +0100

    createItem function

--- src/createItem.js
+++ src/createItem.js
@@ -1,21 +1,21 @@
-// Build a lambda function to insert an Item into dynamodb table
-import { DynamoDB } from 'aws-sdk';
-const docClient = new DynamoDB.DocumentClient();
-import { v4 } from 'uuid';
+'use strict'
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+const uuid = require('uuid');
 const TABLE_NAME = process.env.TABLE_NAME;
 
 
-export async function handler(event) {
+module.exports.handler = async (event) => {
   console.log(event);
   const data = JSON.parse(event.body);
   const params = {
     TableName: TABLE_NAME,
     Item: {
-      id: v4(),
+      id: uuid.v4(),
       ...data
     }
   };
-
+  
   await docClient.put(params).promise();
 
   return {
@@ -27,4 +27,4 @@ export async function handler(event) {
       message: 'Item inserted successfully'
     })
   };
-}
+};
