2a7d9ed0e2719fe8442b5e8ebdbdaae25ebd3cca
vernyuy <fonchu.e.venyuy@gmail.com>
2024-02-22 14:45:03 +0100

    config CI CD

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports.handler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
+      
       {
         message: "Go Serverless v3.0! Your function executed successfully!",
         input: event,
